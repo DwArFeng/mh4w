f638d5c2121cae03ce9b8089673de380b10d334a
DwArFeng <915724865@qq.com>
2017-02-15 21:49:15 +0800

    20170215 Commit A
    Signed-off-by: DwArFeng <915724865@qq.com>

--- src/com/dwarfeng/jier/mh4w/core/view/gui/MainFrame.java
+++ src/com/dwarfeng/jier/mh4w/core/view/gui/MainFrame.java
@@ -12,6 +12,12 @@ import com.dwarfeng.jier.mh4w.core.model.eum.LabelStringKey;
 import com.dwarfeng.jier.mh4w.core.model.struct.Mutilang;
 import com.dwarfeng.jier.mh4w.core.model.struct.MutilangSupported;
 import com.dwarfeng.jier.mh4w.core.view.obv.MainFrameObverser;
+import com.dwarfeng.dutil.basic.gui.swing.JImagePanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JToggleButton;
 
 /**
  * 程序的主界面。
@@ -30,6 +36,55 @@ public final class MainFrame extends JFrame implements MutilangSupported, Obvers
 	 * 新实例。
 	 */
 	public MainFrame() {
+		setResizable(false);
+		setBounds(100, 100, 406, 275);
+		
+		getContentPane().setLayout(null);
+		
+		JImagePanel imagePanel = new JImagePanel();
+		imagePanel.setOpaque(false);
+		imagePanel.setBounds(25, 17, 150, 150);
+		getContentPane().add(imagePanel);
+		
+		JImagePanel imagePanel_1 = new JImagePanel();
+		imagePanel_1.setBounds(225, 17, 150, 150);
+		getContentPane().add(imagePanel_1);
+		
+		JLabel label = new JLabel();
+		label.setText((String) null);
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setFont(new Font("Dialog", Font.BOLD, 16));
+		label.setBounds(225, 167, 150, 25);
+		getContentPane().add(label);
+		
+		JButton button = new JButton();
+		button.setText((String) null);
+		button.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		button.setBounds(225, 200, 150, 40);
+		getContentPane().add(button);
+		
+		JToggleButton toggleButton = new JToggleButton();
+		toggleButton.setToolTipText((String) null);
+		toggleButton.setBounds(135, 200, 40, 40);
+		getContentPane().add(toggleButton);
+		
+		JLabel label_1 = new JLabel();
+		label_1.setText((String) null);
+		label_1.setHorizontalAlignment(SwingConstants.CENTER);
+		label_1.setFont(new Font("Dialog", Font.BOLD, 16));
+		label_1.setBounds(25, 167, 150, 25);
+		getContentPane().add(label_1);
+		
+		JButton button_1 = new JButton();
+		button_1.setToolTipText((String) null);
+		button_1.setBounds(80, 200, 40, 40);
+		getContentPane().add(button_1);
+		
+		JButton button_2 = new JButton();
+		button_2.setToolTipText((String) null);
+		button_2.setOpaque(true);
+		button_2.setBounds(25, 200, 40, 40);
+		getContentPane().add(button_2);
 		
 	}
 
@@ -109,5 +164,4 @@ public final class MainFrame extends JFrame implements MutilangSupported, Obvers
 			if(Objects.nonNull(obverser)) obverser.fireWindowClosing();
 		}
 	}
-
 }
